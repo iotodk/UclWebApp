ad0066544af6706a8103f4e261dc600707118b6e
Ion Toporan <ioto@eal.dk>
2018-10-10 18:38:06 +0200

    Code from class

---
+++ UclWebLib/Data/Data.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UclWebLib.Objects;
+
+namespace UclWebLib.Data
+{
+    public static class Data
+    {
+
+        public static string GetStudentsAsJson(string filePath)
+        {
+            //TODO
+            //read the file
+            //parse it
+            //return it
+            return string.Empty;
+        }
+
+        public static string GetStudentAsJson()
+        {
+            Student student = new Student()
+            {
+                CPR = 12345678,
+                Name = "Io To",
+                Email = "ioto@ucl.dk",
+                Phone = 23547589,
+                Address = new Address("Seeblasgade 1", 5000, "Odense", "Denmark")
+            };
+            return JsonConvert.SerializeObject(student);
+        }
+    }
+}
---
+++ UclWebLib/Objects/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UclWebLib.Objects
+{
+    class Test
+    {
+        public void Test1()
+        {
+        }
+    }
+}
